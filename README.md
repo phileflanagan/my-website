af91199712033945a7cbc52c2bcfb07fe0da87b3
Phil Flanagan <phileflanagan@gmail.com>
2016-06-21 16:44:37 -0400

    Add Keyboard Arrow Key Functionality and Modify Scroll Implementation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [philunfiltered.com](http://www.philunfiltered.com/)
+
+**Known Issues**
+* Touchpad/trackpad Scrolling
+* Lack of Implementation for mobile swiping
--- js/app.js
+++ js/app.js
@@ -1,89 +1,106 @@
-$(function() {
-  // Ranks for Skills in Section 2
-  var skills = [
-    10, 10, 9, 8, 8, 6, 6, 4, 4, 10
-  ]
+	$(function() {
+	  // Ranks for Skills in Section 2
+	  var skills = [
+	    10, 10, 9, 8, 8, 6, 6, 4, 4, 10
+	  ]
 
-  // Set Skill's Rank
-  $('.rank').each(function(i) {
-    $(this).html(convertToBullets(skills[i]));
-  });
+	  // Set Skill's Rank
+	  $('.rank').each(function(i) {
+	    $(this).html(convertToBullets(skills[i]));
+	  });
 
-  // Handle Arrow Clicks
-  var section = 1;
-  $('.down').click(downFn);
-  $('.up').click(upFn);
+	  // Handle Arrow Clicks
+	  var section = 1;
+	  $('.down').click(downFn);
+	  $('.up').click(upFn);
 
-  // Handle Scroll Event
-  var move = true;
-  $(window).on('mousewheel', function(e) {
-    if (move) {
-      if (e.originalEvent.deltaY > 0) {
-        downFn();
-      } else if (e.originalEvent.deltaY < 0) {
-        upFn();
-      }
-      move = false;
-      $.data(this, 'timer', setTimeout(function() {
-        move = true;
-      }, 300));
-    }
-  });
+	  // Handle Scroll Event
+	  $(window).on('mousewheel', function foo(e) {
+	    $this = $(this);
+	    var mousemove = e.originalEvent.wheelDeltaY;
+	    console.log(mousemove);
+	    if (mousemove < 0) {
+	      downFn();
+					    $this.off('mousewheel');
+	    } else if (mousemove > 0) {
+	      upFn();
+					    $this.off('mousewheel');
+	    }
 
-  // Show/Hide Arrows
-  function arrows() {
-    if (section === 1) {
-      $('.up').hide();
-      $('.down').show();
-    } else if (section === 6) {
-      $('.down').hide();
-      $('.up').show();
-    } else {
-      $('.up').show();
-      $('.down').show();
-    }
-    // colors
-    if (section === 5) {
-      $('.up, .down').css({
-        'color': '#ccc'
-      })
-    } else {
-      $('.up, .down').css({
-        'color': 'white'
-      })
-    }
-  }
+	    $.data(this, 'timer', setTimeout(function() {
+	    	move = true;
+	    	$this.on('mousewheel', foo);
+	    }, 300));
 
-  // Page When Scroll Down
-  function downFn() {
-    if (section === 6) return;
-    if (section < 7) section += 1;
-    $('.section' + section).css({
-      'transform': 'translate(0,0)'
-    })
-    arrows();
-  }
+	  });
 
-  // Page When Scroll Up
-  function upFn() {
-    if (section === 1) return;
-    $('.section' + section).css({
-      'transform': 'translate(0,+100vh)'
-    })
-    if (section > 1) section -= 1;
-    arrows();
-  }
 
-  // Convert Numbers to Styled Bullet List
-  function convertToBullets(x) {
-    strFull = '';
-    strDull = '';
-    for (var i = 0; i < x; i++) {
-      strFull += '&bull; ';
-    }
-    for (var i = 0; i < (10 - x); i++) {
-      strDull += '&bull; ';
-    }
-    return '<span class="full">' + strFull + '</span> <span class="dull">' + strDull + '</span>';
-  }
-})
+	  // Handle Keyboard Arrow Key Press
+	  $(window).keydown(function(e) {
+	    switch (e.which) {
+	      case 38: // up arrow
+	        upFn();
+	        break;
+	      case 40: // down arrow
+	        downFn();
+	        break;
+	    }
+	  });
+
+	  // Show/Hide Arrows
+	  function arrows() {
+	    if (section === 1) {
+	      $('.up').hide();
+	      $('.down').show();
+	    } else if (section === 6) {
+	      $('.down').hide();
+	      $('.up').show();
+	    } else {
+	      $('.up').show();
+	      $('.down').show();
+	    }
+	    // colors
+	    if (section === 5) {
+	      $('.up, .down').css({
+	        'color': '#ccc'
+	      })
+	    } else {
+	      $('.up, .down').css({
+	        'color': 'white'
+	      })
+	    }
+	  }
+
+	  // Page When Scroll Down
+	  function downFn() {
+	    if (section === 6) return;
+	    if (section < 7) section += 1;
+	    $('.section' + section).css({
+	      'transform': 'translate(0,0)'
+	    })
+	    arrows();
+	  }
+
+	  // Page When Scroll Up
+	  function upFn() {
+	    if (section === 1) return;
+	    $('.section' + section).css({
+	      'transform': 'translate(0,+100vh)'
+	    })
+	    if (section > 1) section -= 1;
+	    arrows();
+	  }
+
+	  // Convert Numbers to Styled Bullet List
+	  function convertToBullets(x) {
+	    strFull = '';
+	    strDull = '';
+	    for (var i = 0; i < x; i++) {
+	      strFull += '&bull; ';
+	    }
+	    for (var i = 0; i < (10 - x); i++) {
+	      strDull += '&bull; ';
+	    }
+	    return '<span class="full">' + strFull + '</span> <span class="dull">' + strDull + '</span>';
+	  }
+	})
